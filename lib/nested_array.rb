d359a5ba4c3725e15686054055bef1adbfcb3807
Theresa Cyrus <cyrustheresa@gmail.com>
2019-10-28 13:52:07 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,8 +17,7 @@ ENFIELD_TENNIS_ACADEMY = [
 ]
 
 def assembled_matrix
-  # Build an array that contains (or, "nests") the residents of The Ennet House
-  # and the Enfield Tennis Academy as provided by the constants
+[ENNET_HOUSE]
 end
 
 def array_literal_matrix
